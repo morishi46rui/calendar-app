ee7edaf1ef6f506d8ad4c3a158663245599bd701
宮森 瑠偉 <r.miyamori@eisansystem.jp>
2023-08-21 21:14:10 +0900

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Component } from 'react'
+import Button from '@material-ui/core/Button'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Calendar-App</h1>
-    </div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          ></meta>
+          <Button variant="contained" color="primary">
+            Hello World!
+          </Button>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App
